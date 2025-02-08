35955061c598da70a102e9f2a2fbaea0866a05d4
khalidnbg <khalidnabgaoui4@gmail.com>
2025-02-08 20:46:37 +0100

    .

--- src/app/new-listing/[orgId]/page.tsx
+++ src/app/new-listing/[orgId]/page.tsx
@@ -12,14 +12,11 @@ export default async function NewListingForOrgPage(props: PageProps) {
   if (!user) return "please log in";
 
   const workos = new WorkOS(process.env.WORKOS_API_KEY);
-
   const orgId = props.params.orgId;
-
   const oms = await workos.userManagement.listOrganizationMemberships({
     userId: user.id,
     organizationId: orgId,
   });
-
   const hasAccess = oms.data.length > 0;
 
   if (!hasAccess) return "no access";
